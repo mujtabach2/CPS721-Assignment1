58d1bd599f16b619b9cdad1cb508f26c3c4aa73b
Abdulrehman Saleh <125839917+abdulrehmansaleh@users.noreply.github.com>
2025-09-21 22:41:08 -0400

    Update q1_movie_kb.pl

--- q1_movie_kb.pl
+++ q1_movie_kb.pl
@@ -23,9 +23,34 @@
 %%%%% SECTION: q1_kb
 %%%%% You should put the atomic statements for your KB below
 
-releaseInfo()
 
 
+releaseInfo(inception, 2010, 148).
+releaseInfo(titanic, 1997, 195).
+releaseInfo(the_dark_knight, 2008, 152).
+releaseInfo(the_godfather, 1972, 175).
+releaseInfo(avengers_endgame, 2019, 181).
+releaseInfo(gladiator, 2000, 155).
+releaseInfo(pulp_fiction, 1994, 154).
+releaseInfo(the_matrix, 1999, 136).
+releaseInfo(parasite, 2019, 132).
+releaseInfo(joker, 2019, 122).
+
+directedBy(inception, christopher_nolan).
+directedBy(titanic, james_cameron).
+directedBy(the_godfather, francis_ford_coppola).
+directedBy(avengers_endgame, anthony_russo).
+directedBy(avengers_endgame, joe_russo).
+directedBy(parasite, bong_joon_ho).
+directedBy(joker, todd_phillips).
+
+actedIn(leonardo_dicaprio, inception, dom_cobb).
+actedIn(kate_winslet, titanic, rose_dewitt_bukater).
+actedIn(marlon_brando, the_godfather, vito_corleone).
+actedIn(robert_downey_jr, avengers_endgame, tony_stark).
+actedIn(keanu_reeves, the_matrix, neo).
+actedIn(song_kang_ho, parasite, kim_ki-taek).
+actedIn(joaquin_phoenix, joker, arthur_fleck).
 
 
 
